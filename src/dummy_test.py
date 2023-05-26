497b5818802b1e2dea0c1d24e5a7dbc69bcb47fc
Max Bramberger <max.bramberger@tngtech.com>
2023-05-26 10:54:21 +0200

    Add failing test

--- src/dummy_test.py
+++ src/dummy_test.py
@@ -3,3 +3,7 @@ import pandas as pd
 
 def test_create_df() -> None:
     df = pd.DataFrame({'column_name': [1, 2, 3]})
+
+
+def test_failing_test() -> None:
+    df = pd.DataFrame( {'afafafafaf'}, 'afaf')
