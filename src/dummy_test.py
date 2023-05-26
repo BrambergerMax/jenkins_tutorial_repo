1aaf89e433deea9b20950fe3a47508608a88cb89
Max Bramberger <max.bramberger@tngtech.com>
2023-05-26 11:54:14 +0200

    fix test

--- src/dummy_test.py
+++ src/dummy_test.py
@@ -5,5 +5,5 @@ def test_create_df() -> None:
     df = pd.DataFrame({'column_name': [1, 2, 3]})
 
 
-def test_failing_test() -> None:
-    df = pd.DataFrame( {'afafafafaf'}, 'faf')
+def test_fixed_test() -> None:
+    df = pd.DataFrame( {'afafafafaf': ["works"]})
